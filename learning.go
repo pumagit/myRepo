26c6037089601997c26740b33704e79d68b5679a
pumagit <pumastuff@gmail.com>
2017-01-07 14:23:31 -0800

    Define Empty slices; Append and Copy functions

--- learning.go
+++ learning.go
@@ -6,8 +6,13 @@ func main() {
 	numSlice := []int {5,4,3,2,1}
 
 
+	// Slices with defined dimension
 	numSlice3 := make([]int,5,10)
-	copy(numSlice3,numSlice)
+	copy(numSlice3,numSlice) // copy slice to slice
 	fmt.Println(numSlice[0])
+
+	// Append element to an slice
+	numSlice3 = append(numSlice3,0,-2)
+	fmt.Println(numSlice3[6])
 
 }
